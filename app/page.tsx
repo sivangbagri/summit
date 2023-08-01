dab0f8e978c190f0595c7dae852ffedd0d967381
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-08-01 15:48:46 +0530

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 // import HeroSection from "@/components/normal/hero";
 import Navbar from "@/components/main-nav";
 import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <Footer />
     </main>
   );
 }
