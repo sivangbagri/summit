a7e1703acb36ba5fe66d413064ecace87e7a2030
Vishal K <116670999+Vishalk91-4@users.noreply.github.com>
2023-08-12 01:04:00 +0530

    Updated Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,27 +3,38 @@ import Image from "next/image";
 import React from "react";
 import Link from "next/link";
 import { Icons } from "@/components/icons";
-
-
-
+import cell from "@/public/logo/E-Cell.png";
+import styles from './styles.module.css';
 const Footer: React.FC = () => {
-
+  /* useEffect(() => {
+    // After the component mounts, add a class to trigger the animation
+    const animatedDiv = document.getElementById('animatedDiv');
+    if (animatedDiv) {
+      animatedDiv.classList.add('animate'); // Assuming you have a class named 'slide-in' for animation
+    }
+  }, []); */
   return (
     <section className="mt-10">
 
         <div className="justify-content mx-auto h-auto w-11/12 gap-14 md:flex">
         <div className="mt-16 basis-1/4 md:mt-0">
             <h4 className="font-bold text-center text-primary">E-Cell IIT BHU</h4>
-            <div className="w-1/3 h-3 bg-teal-600 rounded relative">
+            <div id="borders" className={`${styles.borders} ${styles.animate}`}>
               <span></span>
             </div>
-            <Link href="/">
-              <Image width={200} height={200} src="/E-cell.png" alt="logo"/>
-            </Link>
+            <div>
+                            <Image
+                                className={styles.imageContainer}
+                                src={cell}
+                                alt="ecell"
+                                loading="lazy"
+                                width="200"
+                                height="200"
+                            />            </div>
         </div>
         <div className="mt-16 basis-1/4 md:mt-0">
             <h4 className="font-bold text-center text-primary">Contact Us</h4>
-            <div className="w-1/3 h-3 bg-teal-600 rounded relative">
+            <div id="borders" className={`${styles.borders} ${styles.animate}`}>
               <span></span>
             </div>
             <p className=" mt-5 text-center">
@@ -37,10 +48,10 @@ const Footer: React.FC = () => {
          </div>
          <div className="mt-16 basis-1/4 md:mt-0">
             <h4 className="font-bold text-center text-primary">Quick Links</h4>
-            <div className="w-1/3 h-3 bg-teal-600 rounded relative">
-              <p></p>
+            <div id="borders" className={`${styles.borders} ${styles.animate}`}>
+              <span></span>
             </div>
-            <div className="text-left pl-20">
+            <div className="text-center pt-5">
             <div><a href="/home">Home</a></div> 
         <div className="py-2"><a href="/about">About </a></div>
         <div className="pb-2"><a href="/guide">Guide</a></div>
@@ -50,12 +61,12 @@ const Footer: React.FC = () => {
          </div>
         <div className="mt-16 basis-1/4 md:mt-0">
         <div className="font-bold text-center text-primary">Connect with Us
-        <div className="w-1/3 h-3 bg-teal-600 rounded relative">
-          <span></span>
-        </div>
+        <div id="borders" className={`${styles.borders} ${styles.animate}`}>
+              <span></span>
+            </div>
       </div>
-      <div className="flex justify-start m-5 p-0"></div>
-      <div className="text-base h-12 w-12 inline-block text-center rounded">
+      <div className="flex justify-center items-center m-5 p-0">
+      <div className="text-base h-12 w-120 inline-block text-center rounded ">
       <ul className="footer-menu">
           <li className="footer-item">
             <Link
@@ -94,6 +105,7 @@ const Footer: React.FC = () => {
             </Link>
           </li>
         </ul>
+      </div>
       </div>
       </div>
       </div>
