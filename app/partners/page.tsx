0efd33f16b5a7ec5175f1b251fb9ca5f39e9d37b
Ashvita Ramanan <r.ashvita.mec22@itbhu.ac.in>
2023-08-12 14:55:58 +0530

    cooming soon on partners

---
+++ app/partners/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function partners(){
+    return(
+        <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
+    <h1 className="text-5xl text-white font-bold mb-8 animate-pulse">
+        Coming Soon
+    </h1>
+</div>
+    )
+}
+export default partners;
