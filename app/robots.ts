8dc4dfb9ed969555abe22d2b6772440054524e89
rahulX001 <rahul.kumarsonkar.eee21@itbhu.ac.in>
2023-08-16 00:44:01 +0530

    some seo

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://startupsummit23.ecelliitbhu.com",
+    sitemap: "https://startupsummit23.ecelliitbhu.com/sitemap.xml",
   };
 }
